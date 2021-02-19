e611533f74ad2a98b4137dbfc6482402604e15c2
alexgabriel2 <49072620+alexgabriel2@users.noreply.github.com>
2021-02-19 17:40:24 +0200

    fa

--- dependencies/functions.php
+++ dependencies/functions.php
@@ -27,24 +27,40 @@ function validpassword($password) {
         return "0";
     }
 }
+
 function emailexist($conn, $email){
+
     $sqlusers="SELECT email FROM users WHERE email=?;";
-    #$sqlorg ="SELECT * FROM org WHERE email=?;";
+    $sqlorg ="SELECT email FROM org WHERE email=?;";
+
     $stmt=mysqli_stmt_init($conn);
+    $stmt2=mysqli_stmt_init($conn);
+
     mysqli_stmt_prepare($stmt,$sqlusers);
+    mysqli_stmt_prepare($stmt2,$sqlorg);
 
     mysqli_stmt_bind_param($stmt,"s",$email);
+    mysqli_stmt_bind_param($stmt2,"s",$email);
+
     mysqli_stmt_execute($stmt);
+    mysqli_stmt_execute($stmt2);
+
     $resultuser=mysqli_stmt_get_result($stmt);
-    if(mysqli_fetch_assoc($resultuser)){
+    $resultorgr=mysqli_stmt_get_result($stmt2);
+
+    if(mysqli_fetch_assoc($resultuser) || mysqli_fetch_assoc($resultorgr)){
         return "1";
         #exista
     }
+
     mysqli_stmt_close($stmt);
+    mysqli_stmt_close($stmt2);
+
 }
+
 function registeruser($conn, $nume, $prenume, $email, $password){
 
-    $sql = "INSERT INTO users (nume, prenume, email, password, verified, regkey, ipreg) VALUE (?, ?, ?, ?, ?, ?, ?);";
+    $sql = "INSERT INTO users (nume, prenume, email, password, verified, regkey, ipreg, countryreg,cityreg,regionreg) VALUE (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
     $sqluser = "INSERT INTO logusr (email, password, user) VALUE (?, ?, ?);";
 
     $stmt = mysqli_stmt_init($conn);
@@ -56,7 +72,13 @@ function registeruser($conn, $nume, $prenume, $email, $password){
     $verified=0;
     $key=$nume.time().$prenume;
     $user=true;
+
     $ip = $_SERVER['REMOTE_ADDR'];
+    $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}"));
+	$country = $details->country;
+    $city= $details->city;
+    $region=$details->region;
+    
     $options = [
         'cost' => 12,
     ];
@@ -64,13 +86,63 @@ function registeruser($conn, $nume, $prenume, $email, $password){
     $verifykey=hash('sha256',$key);
     $hashedpsw=password_hash($password,PASSWORD_BCRYPT, $options);
 
-    mysqli_stmt_bind_param($stmt, "sssssss", $nume, $prenume, $email, $hashedpsw, $verified, $verifykey, $ip);
+    mysqli_stmt_bind_param($stmt, "ssssssssss", $nume, $prenume, $email, $hashedpsw, $verified, $verifykey, $ip, $country, $city, $region);
     mysqli_stmt_bind_param($stmt2, "sss", $email, $hashedpsw, $user);
 
     mysqli_stmt_execute($stmt);
     mysqli_stmt_execute($stmt2);
 
     $usr="volutar";
+    $to=$email;
+    $subject ="Email Verification";
+    $message ="<a href='http://localhost/bvolunteer/account/verify.php?verifykey=$verifykey&is=$usr'>Verify your account</a>";
+    $header = "From: cloudgreen2020@gmail.com \r\n";
+    $header .= "MIME-Version: 1.0"."\r\n";
+    $header .= "Content-type:text/html;charset=UTF-8"."\r\n";
+
+    mail($to,$subject,$message,$header);
+
+    mysqli_stmt_close($stmt);
+    mysqli_stmt_close($stmt2);
+
+}
+
+function registerorg($conn, $nume, $prenume, $email, $password){
+
+    $sql = "INSERT INTO org (nume, prenume, email, password, verified, regkey, ipreg, countryreg, cityreg, regionreg) VALUE (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+    $sqluser = "INSERT INTO logusr (email, password, user) VALUE (?, ?, ?);";
+
+    $stmt = mysqli_stmt_init($conn);
+    $stmt2 = mysqli_stmt_init($conn);
+
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_prepare($stmt2, $sqluser);
+
+    $verified=0;
+    $key=$nume.time().$prenume;
+
+    $user=false;
+
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}"));
+	$country = $details->country;
+    $city= $details->city;
+    $region=$details->region;
+
+    $options = [
+        'cost' => 12,
+    ];
+
+    $verifykey=hash('sha256',$key);
+    $hashedpsw=password_hash($password, PASSWORD_BCRYPT, $options);
+
+    mysqli_stmt_bind_param($stmt, "ssssssssss", $nume, $prenume, $email, $hashedpsw, $verified, $verifykey, $ip, $country, $city, $region);
+    mysqli_stmt_bind_param($stmt2, "sss", $email, $hashedpsw, $user);
+
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_execute($stmt2);
+
+    $usr="org";
     $to=$email;
     $subject ="Email Verification";
     $message ="<a href='http://localhost/bvolunteer/account/verify.php?verifykey=$verifykey&is=$usr'>Verify your account</a>";
@@ -93,19 +165,23 @@ function login($conn,$email,$password){
 
     mysqli_stmt_bind_param($stmt,"s",$email);
     mysqli_stmt_execute($stmt);
+
     $result=mysqli_stmt_get_result($stmt);
     $fetreuslt=mysqli_fetch_assoc($result);
+
     if($fetreuslt){
-       $hash=$fetreuslt['password'];
-       if(password_verify($password,$hash)){
+        $hash=$fetreuslt['password'];
+        if(password_verify($password,$hash)){
             return "1";
-       }
-       else{
+        }
+        else{
            return "2";
-       }
+        }
     }
     else{
         return "2";
-   }
+    }
+
     mysqli_stmt_close($stmt);
+
 }
---
+++ register/preregisterorg.php
@@ -0,0 +1,153 @@
+<?php 
+
+require_once '../dependencies/db_connect.php';
+require_once '../dependencies/functions.php';
+
+if (isset($_POST['submit'])){
+
+    $nume=$_POST['nume'];
+    $email=$_POST['email'];
+    $ong=$_POST['companie'];
+    $phonenumber=$_POST['number'];
+    $password=$_POST['password'];
+    $confirmpassword=$_POST['confirmpassword'];
+
+    $error = false;
+    $empty_nume = false;
+    $invalid_nume = false;
+    $empty_prenume = false;
+    $invalid_prenume = false;
+    $empty_email = false;
+    $invalid_email = false;
+    $email_exist = false;
+    $empty_password = false;
+    $nrcha_password = false;
+    $number_password = false;
+    $capital_password = false;
+    $lowercase_password = false;
+    $special_password = false;
+    $empty_confirmpassword = false;
+    $passworddiff = false;
+
+    if(empty($nume)){
+        $error = true; $empty_nume = true;
+    }
+    elseif(validnume($nume)){
+        $error = true; $invalid_nume = true;
+    }
+    if(empty($prenume)){
+        $error=true; $empty_prenume=true;
+    }
+    elseif(validprenume($prenume)){
+        $error=true; $invalid_prenume=true;
+    }
+    if(empty($email)){
+        $error=true; $empty_email=true;      
+    }
+    elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $error=true; $invalid_email=true; 
+    }
+    elseif(emailexist($conn, $email)==1){
+        $error=true; $email_exist=true;
+    }
+    if(empty($password)){
+        $error=true; $empty_password=true;     
+    }
+    if(validpassword($password)==1){
+        $error=true; $nrcha_password=true;       
+    }
+    if(validpassword($password)==2){
+        $error=true; $number_password=true;
+    }
+    if(validpassword($password)==3){
+        $error=true; $lowercase_password=true;
+    }
+    if(validpassword($password)==4){
+        $error=true; $capital_password=true;
+    }
+    if(validpassword($password)==5){
+        $error=true; $special_password=true;
+    } 
+    if(empty($confirmpassword)){
+        $error=true; $empty_confirmpassword= true;
+    }
+    if($password !==$confirmpassword){
+        $error=true; $passworddiff=true;
+    }
+    #execute actual code
+    if($error==false){
+        registerorg($conn, $nume, $prenume, $email, $password);
+    }
+    
+}
+else {
+    header("Location: register.php");
+}
+?>
+<script>
+    var emptyNume = "<?php echo $empty_nume; ?>";
+    var invalidNume = "<?php echo $invalid_nume; ?>";
+    var emptyPrenume = "<?php echo $empty_prenume; ?>";
+    var invalidPrenume = "<?php echo $invalid_prenume; ?>";
+    var emptyEmail = "<?php echo $empty_email; ?>";
+    var invalidEmail = "<?php echo $invalid_email; ?>";
+    var existEmail = "<?php echo $email_exist; ?>";
+    var emptyPassword = "<?php echo $empty_password; ?>";
+    var emptyConfirmPassword = "<?php echo $empty_confirmpassword; ?>";
+    var nrchaPassword = "<?php echo $nrcha_password; ?>";
+    var numberPassword = "<?php echo $number_password; ?>";
+    var capitalPassword = "<?php echo $capital_password; ?>";
+    var lowecasePassword = "<?php echo $lowercase_password; ?>";
+    var specialPassword = "<?php echo $special_password; ?>";
+    var diffPassword = "<?php echo $passworddiff; ?>";
+    var error = false;
+
+    if(emptyNume == true){
+        error=true;
+    }
+    if(invalidNume==true){
+        error=true;
+    }
+    if(emptyPrenume==true){
+        error=true;
+    }
+    if(invalidPrenume==true){
+        error=true;
+    }
+    if(emptyEmail==true){
+        error=true;
+    }
+    if(invalidEmail==true){
+        error=true;
+    }
+    if(existEmail==true){
+        error=true;
+    }
+    if(emptyPassword==true){
+        error=true;
+    }
+    if(nrchaPassword==true){
+        error=true;
+    }
+    if(numberPassword==true){
+        error=true;
+    }
+    if(capitalPassword==true){
+        error=true;
+    }
+    if(lowecasePassword==true){
+        error=true;
+    }
+    if(specialPassword==true){
+        error=true;
+    }
+    if(emptyConfirmPassword==true){
+        error=true;
+    }
+    if(diffPassword==true){
+        error=true;
+    }
+    if(error==false){
+        
+    }
+</script>
